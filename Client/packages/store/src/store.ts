2a4fa27eceb2a7d5955aa01811aaab8056831f78
Ashwin <ashwin.2201098cs@iiitbh.ac.in>
2025-12-27 14:45:00 +0530

    fix: Correct import statement for userReducer by removing file extension

--- Client/packages/store/src/store.ts
+++ Client/packages/store/src/store.ts
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { useDispatch, useSelector, TypedUseSelectorHook } from 'react-redux';
-import userReducer from './slices/userSlice.js';
+// @ts-ignore
+import userReducer from './slices/userSlice';
 
 export const store = configureStore({
     reducer: {
