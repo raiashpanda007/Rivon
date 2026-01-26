fdb399ded5e33a1eba57c2d928ccf75725861c6b
Ashwin <ashwin.2201098cs@iiitbh.ac.in>
2026-01-26 18:15:08 +0530

    feat(auth): Implement user authentication check in AppLayout and update SignIn button behavior feat(header): Enhance Options component with navigation to external URLs style: Add font variables to global CSS for improved typography

--- Client/packages/ui/src/components/Header/Options.tsx
+++ Client/packages/ui/src/components/Header/Options.tsx
@@ -1,13 +1,16 @@
+"use client"
 import { BsGraphUp } from 'react-icons/bs';
 import { MdAttachMoney } from 'react-icons/md';
 import { Button } from "@workspace/ui/components/button";
-
+import { useRouter } from 'next/navigation';
 function Options() {
+  const router = useRouter();
   return (
     <div className="flex items-center gap-2">
       <Button
         variant="ghost"
         className="gap-2 font-medium text-muted-foreground hover:text-foreground hover:bg-muted/50 transition-all cursor-pointer"
+        onClick={() => router.push(process.env.NEXT_PUBLIC_BET_APP_URL ?? "")}
       >
         <MdAttachMoney className="text-lg" />
         Betting
@@ -15,6 +18,7 @@ function Options() {
       <Button
         variant="ghost"
         className="gap-2 font-medium text-muted-foreground hover:text-foreground hover:bg-muted/50 transition-all cursor-pointer"
+        onClick={() => router.push(process.env.NEXT_PUBLIC_TRADE_APP_URL ?? "")}
       >
         <BsGraphUp className="text-lg" />
         Trading
--- Client/packages/ui/src/components/Header/authOptions/SignIn.tsx
+++ Client/packages/ui/src/components/Header/authOptions/SignIn.tsx
@@ -3,8 +3,11 @@ import { Button } from "@workspace/ui/components/button";
 import { useRouter } from "next/navigation.js";
 function SignIn() {
   const router = useRouter();
+
+  const loginUrl = (process.env.NEXT_PUBLIC_BASE_APP_URL || "/") + "auth/login";
+
   return (
-    <Button variant="ghost" onClick={() => router.push("/auth/login")} className="font-medium gap-2 cursor-pointer">
+    <Button variant="ghost" onClick={() => window.location.href = loginUrl} className="font-medium gap-2 cursor-pointer">
       Sign in
     </Button>
   )
